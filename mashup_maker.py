f6724bea4d65e67f42d597b6791f16e24cd606b7
dviralfi <dviralafiprogram@gmail.com>
2022-03-15 00:33:06 +0200

    Adding Vocal Remover 5 Components

--- mashup_maker.py
+++ mashup_maker.py
@@ -1,5 +1,5 @@
 import argparse
-import 
+import inference
 
 
 
@@ -8,19 +8,19 @@ PARSING_DESCRIPTION_TEXT = "Enter your songs for mashuping:" \
                             "-b - Path to base song.\n" \
                             "-v - Path to vocal song.\n"
 
-def main(base_song,vocal_song):
-    pass
+def main(base_song_path,vocal_song_path):
+    inference.main(input=vocal_song_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=PARSING_DESCRIPTION_TEXT)
-    parser.add_argument('-b', "--base_song", help="Path to base song")
-    parser.add_argument('v','--vocal_song', help="Path to vocal song")
+    parser.add_argument('-b', "--base_song_path", help="Path to base song")
+    parser.add_argument('v','--vocal_song_path', help="Path to vocal song")
 
     args = parser.parse_args()
 
-    base_song = args.base_song
-    vocal_song = args.vocal_song
+    base_song_path = args.base_song_path
+    vocal_song_path = args.vocal_song_path
    
 
-    main(base_song,vocal_song)
+    main(base_song_path,vocal_song_path)
