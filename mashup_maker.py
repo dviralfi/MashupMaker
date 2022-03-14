3c91f9270ddca51f66001bf9f14b0490e584256b
dviralfi <dviralafiprogram@gmail.com>
2022-03-15 01:10:57 +0200

    fixing adding vr5 comps

--- mashup_maker.py
+++ mashup_maker.py
@@ -3,16 +3,20 @@ import inference
 
 
 
-
 PARSING_DESCRIPTION_TEXT = "Enter your songs for mashuping:" \
                             "-b - Path to base song.\n" \
                             "-v - Path to vocal song.\n"
 
-def main(base_song_path,vocal_song_path):
-    inference.main(input=vocal_song_path)
+#def main(base_song_path,vocal_song_path):
+def main():
+    vocal_song_path = 'C:/Users/refae/Desktop/song1'
+    inference.main(vocal_song_path)
 
 
 if __name__ == '__main__':
+    main()
+
+
     parser = argparse.ArgumentParser(description=PARSING_DESCRIPTION_TEXT)
     parser.add_argument('-b', "--base_song_path", help="Path to base song")
     parser.add_argument('v','--vocal_song_path', help="Path to vocal song")
@@ -21,6 +25,6 @@ if __name__ == '__main__':
 
     base_song_path = args.base_song_path
     vocal_song_path = args.vocal_song_path
-   
-
+    
     main(base_song_path,vocal_song_path)
+
