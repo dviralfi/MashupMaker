dfe31cb39eba61288cd8fc473d0ef59e670e8380
dviralfi <dviralafiprogram@gmail.com>
2022-03-22 15:14:30 +0200

    Adding detecting key&scale

--- mashup_maker.py
+++ mashup_maker.py
@@ -12,15 +12,22 @@ Returns: 1 mashup song
 """
 
 import argparse
+
+from functools import reduce
 import inference    # Vocal Remover 5 Open Source Code
 import librosa
 import os
 from itertools import count
+import numpy as np
+from pyACA import computePitch
+import keyfinder
 
 PARSING_DESCRIPTION_TEXT = "Enter your songs for mashuping:" \
                             "-b - Path to base song.\n" \
                             "-v - Path to vocal song.\n"
 CHROMATIC_KEYS = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B')
+PITCH_TRACKERS_LIST = ['SpectralAcf', 'SpectralHps', 'TimeAcf', 'TimeAmdf', 'TimeAuditory', 'TimeZeroCrossings',]
+
 
 """
 def detect_pitch(y, sr, t):
@@ -60,36 +67,51 @@ def get_song_bpm(wave_form, sample_rate):
     # The function returns the first item, because 'beat_track' func returns a tuple, and the second item is beat frams which the program don't need. 
     # Also, the value that been returned is a float and the program need it as an Integer number.
 
-
+import soundfile as sf
 def main(base_song_path,vocal_song_path):
+
 
     # Loading the songs into a computible sound files:
 
     base_wave_form, base_sample_rate = librosa.load(base_song_path)
     vocal_wave_form, vocal_sample_rate = librosa.load(vocal_song_path)
 
-
     # Recognize each song's BPM & Scale 
+
 
         # recognize BPM:
     base_bpm = get_song_bpm(base_wave_form,base_sample_rate)
     vocal_bpm = get_song_bpm(vocal_wave_form,vocal_sample_rate)
     
-    if base_bpm > 150:                  # if the bpm is too fast, the vocals wont be heard properly, and will sound horrible and not human.
+    if base_bpm > 160:                  # if the bpm is too fast, the vocals wont be heard properly, and will sound horrible and not human.
         base_bpm = int(base_bpm / 2)
-    if vocal_bpm > 150:                 # on the other hand if the instruments will be too fast the song will sound bad and not recognizable by the listener.
+    if vocal_bpm > 160:                 # on the other hand if the instruments will be too fast the song will sound bad and not recognizable by the listener.
         vocal_bpm = int(vocal_bpm / 2)
 
     print("Base BPM:",base_bpm,"Vocal BPM:",vocal_bpm)
 
         # Recognize Scale:
-    pass
-    pass
-    pass
 
-    base_scale = "C"
-    vocal_scale = "G#"
+    """
+    pitch_tracker = PITCH_TRACKERS_LIST[2]      # TimeZeroCrossings
 
+    base_f0, base_time_stamp = computePitch(pitch_tracker,base_wave_form,base_sample_rate)
+    vocal_f0, vocal_time_stamp = computePitch(pitch_tracker,vocal_wave_form,vocal_sample_rate)
+
+    print('base freq:\n',base_f0)
+    
+    print('vocal freq:\n',vocal_f0)
+    """
+
+
+    base_obj = keyfinder.Tonal_Fragment(base_wave_form,base_sample_rate)
+    base_f0 = base_obj.get_key()[0].split(' ')[0]
+
+    vocal_obj = keyfinder.Tonal_Fragment(vocal_wave_form,vocal_sample_rate)
+    vocal_f0 = vocal_obj.get_key()[0].split(' ')[0]
+
+
+    print(base_f0,vocal_f0)
 
 
     # Seperating the Vocals and Instruments from each song:
@@ -97,13 +119,25 @@ def main(base_song_path,vocal_song_path):
     #inference.main(base_song_path,is_base=True)
     #inference.main(vocal_song_path,is_base=False)
 
+
+
+    
     # Reading the seperated files :
 
-    base_wave_form, base_sample_rate = librosa.load(str(os.path.dirname(os.path.realpath(base_song_path)))+str(os.path.basename(base_song_path).split('.')[0])+'_Instruments')
-    vocal_wave_form, vocal_sample_rate = librosa.load(str(os.path.dirname(os.path.realpath(vocal_song_path)))+str(os.path.basename(vocal_song_path).split('.')[0])+'_Vocals')
+
+    
+    file_extension = os.path.basename(base_song_path).split('.')[1] 
+    base_wave_form, base_sample_rate = librosa.load(os.getcwd()+'\\'+str(os.path.basename(base_song_path).split('.')[0])+'_Instruments.'+file_extension)
+    #base_wave_form, base_sample_rate = librosa.load(str(os.path.dirname(os.path.realpath(base_song_path)))+'\\'+str(os.path.basename(base_song_path).split('.')[0])+'_Instruments.'+file_extension)
+
+    file_extension = os.path.basename(vocal_song_path).split('.')[1] 
+    vocal_wave_form, vocal_sample_rate = librosa.load(os.getcwd()+'\\'+str(os.path.basename(vocal_song_path).split('.')[0])+'_Vocals.'+file_extension)
+    #vocal_wave_form, vocal_sample_rate = librosa.load(str(os.path.dirname(os.path.realpath(vocal_song_path)))+'\\'+str(os.path.basename(vocal_song_path).split('.')[0])+'_Vocals.'+file_extension)
+    
 
     # Adjusting the base Scale to the vocal Scale: 
-    base_wave_form = librosa.effects.pitch_shift(y=base_wave_form, sr=base_sample_rate, n_steps=abs(CHROMATIC_KEYS.index(base_scale)-CHROMATIC_KEYS.index(vocal_scale)))
+    print(abs(CHROMATIC_KEYS.index(base_f0)-CHROMATIC_KEYS.index(vocal_f0)))
+    base_wave_form = librosa.effects.pitch_shift(y=base_wave_form, sr=base_sample_rate, n_steps=abs(CHROMATIC_KEYS.index(base_f0)-CHROMATIC_KEYS.index(vocal_f0)))
 
     # Adjusting the vocal BPM to the base BPM: 
     
@@ -114,7 +148,11 @@ def main(base_song_path,vocal_song_path):
 
     vocal_wave_form = librosa.effects.time_stretch(y=vocal_wave_form, rate=stretch_rate)
 
-    # Mixing the elements together:
+    
+
+
+
+    # Mixing the seperated elements together:
 
         #Finding unique name for the mushap file:
     file_extension = os.path.basename(vocal_song_path).split('.')[1] 
@@ -129,8 +167,8 @@ def main(base_song_path,vocal_song_path):
 if __name__ == '__main__':
 
     
-    base_song_path = 'C:/Users/refae/Desktop/melody.wav'
-    vocal_song_path = 'C:/Users/refae/Desktop/chords.wav'
+    base_song_path = 'C:/Users/refae/Desktop/pardes.wav'
+    vocal_song_path = 'C:/Users/refae/Desktop/leva.wav'
 
     main(base_song_path,vocal_song_path)
 
